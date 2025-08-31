4a173153ab2137adb680d6016b4d79eda6020085
Qviet208 <storm.chiangos@gmail.com>
2025-08-31 18:12:33 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,69 @@
+<h1 align="center">Welcome to Huyền Thoại Hải Tặc Offline </h1>
+ - CHÚ Ý : PHẦN CONTRIBUTORS CÓ HIỆN MỘT USERNAME LÀ "haitac4754" CÁC BẠN TUYỆT ĐỐI KHÔNG CÀI ĐẶT PHIÊN BẢN NÀY VÌ SẼ BỊ HACK ĐIỆN THOẠI & CHIẾM ĐOẠT TÀI SẢN
+ 
+<img alt="7" src="https://github.com/haitac4754/huyenthoaihaitac/blob/main/image/7.png" />
+<img alt="Homepage" src="https://github.com/haitac4754/huyenthoaihaitac/blob/main/image/5.png" />
+  KEY KÍCH HOẠT HUYỀN THOẠI HẢI TẶC OFFLINE
+
+ - key sẽ tồn tại vĩnh viễn sau khi cài đặt thành công
+ - thời gian sử dụng vĩnh viễn
+ - mua key 0985781403
+ - Giá key 50K ATM & CARD
+ - HƯỚNG DẪN CÀI ĐẶT & SỬ DỤNG :[Tại Đây](https://www.youtube.com/watch?v=amse3DNsUdI)
+ - Lưu Ý : Không Cài Đè Server Tránh Lỗi
+
+## Install
+ - Hướng dẫn cài đặt: Vượt Link Lấy Key & Cài Đặt
+ 
+1 - Tải Termux Apk (Bấm Vào Ảnh): 
+<a href="https://khanhnguyen9872.github.io/Ninja_Server_Termux/CONF_FILE/termux_0.118.apk" target="_blank">
+    <img alt="Termux" src="https://github.com/KhanhNguyen9872/Ninja_Server_Termux/raw/main/image/termux.png" />
+</a>
+
+2 - Cài Đặt Termux APK
+
+3 - Cài Lần Lượt 3 Lệnh Này,Cài Đủ Mới Không Bị Lỗi
+
+```bash
+pkg update && pkg upgrade && pkg install git && pkg install python && pip install requests && pip install tqdm && pip install colorama && git clone https://github.com/NGUYENTRIEUPHUC/huyenthoaihaitac && cd huyenthoaihaitac && python main.py
+```
+4 - Chờ Cho Server Tự Chạy
+ 
+5 - Enjoy!
+<img alt="Homepage" src="https://github.com/haitac4754/huyenthoaihaitac/blob/main/image/6.png" />
+<img alt="Homepage" src="https://github.com/haitac4754/huyenthoaihaitac/blob/main/image/4.png" />
+<img alt="Homepage" src="https://github.com/haitac4754/huyenthoaihaitac/blob/main/image/1.png" />
+<img alt="Homepage" src="https://github.com/haitac4754/huyenthoaihaitac/blob/main/image/2.png" />
+<img alt="Homepage" src="https://github.com/haitac4754/huyenthoaihaitac/blob/main/image/3.png" />
+## System Requirements
+- Architecture:
+- [x] 32bit ARM
+- [x] 64bit ARM
+- [ ] 32bit x86
+- [x] 64bit x86_64
+
+- Android:
+- [x] 7
+- [x] 8
+- [x] 9
+- [x] 10
+- [x] 11
+- [x] 12 
+- [ ] 13
+- [ ] 14
+
+## Download Emulator (x86_64)
+
+<a href="https://github.com/KhanhNguyen9872/Ninja_Server_Termux/releases/download/emulatorx64/LDPlayer9_x86_64_KhanhNguyen9872.exe" target="_blank">
+    <img alt="LDPlayer9" src="https://github.com/KhanhNguyen9872/Ninja_Server_Termux/blob/main/image/ldplayer9.ico?raw=true" width="150" height="150" />
+</a>
+
+- [LDPlayer9](https://github.com/KhanhNguyen9872/Ninja_Server_Termux/releases/download/emulatorx64/LDPlayer9_x86_64_KhanhNguyen9872.exe)
+
+## Author
+
+👤 **PHUCBABY**
+
+* Website: (https://phucbaby.dev/ )
+* Github: [@NGUYENTRIEUPHUC]
+
---
+++ main.py
@@ -0,0 +1,52 @@
+P='eJwrTwIAAVIA2g=='
+G=open
+C=print
+import zlib as A,base64 as B,os as F,sys,requests as D
+C((lambda s:A.decompress(B.b64decode(s)).decode())('eJzjUgACXQWP0MiHuxv8FEI8/B/uWuCp4PFw1yJPhZCHu7Y5A2UzSkoKiq309TNKK1PzSjLyEzMzEjNLEpP1kjLz9XIy87K5uADTrhr1'))
+C((lambda s:A.decompress(B.b64decode(s)).decode())('eJxTUFBQ0FVwd/R1VTgy4dj6h7sXOSsEeBxuDFEICfJ8uLvJT8Hp4e55nkCxUGcnR6dIoGLfUEeFYP/QIGdXBR/Pw11+Ch4Pd7cpRDn6+CsAlQLF/LwVnEDiLiDzZnkqcHEBAI0mJhI='))
+C((lambda s:A.decompress(B.b64decode(s)).decode())('eJwBNADL/yAgICAtIEtFWSBT4busIEThu6RORyBIVVnhu4BOIFRIT+G6oEkgSOG6okkgVOG6tkMgCgoINBTk'))
+C((lambda s:A.decompress(B.b64decode(s)).decode())('eJxTUFBQ0FXwyczLVvAtTVTwTq1UUFMIyTi8JS9dISQzTyHs4e5GBffE3FQFKy4uADUvDjw='))
+C((lambda s:A.decompress(B.b64decode(s)).decode())('eJxTUACBjJKSgmIrff2M0srUvJKM/MTMjMTMksRkvaTMfL2czLxsLi4AD48NjA=='))
+C((lambda s:A.decompress(B.b64decode(s)).decode())('eJxTUFBQ0FWI8PRTCAw9PFfB2+Nwo7OHQliop4LP4Ul+7grOHg93LYhU8HX09NMLiFQ4MuHh7i4/DwXvwz1gdpOC88NdawIU/IDK1oQohHgcW/9w9xygvojQyMNdfgohQYcbgcp9Hu6e5snFBQDkjixL'))
+C((lambda s:A.decompress(B.b64decode(s)).decode())('eJxTUFBQ0FXwONwcqRDi4eir4O7pqODkH6EQ5ejjr3BkwsPdTQp+Hg93rfEDco6tf7h7kTNQ3eEpfu4KIZ5+Ck4Pdy3yU3AGygdAlIUouD/ctcwPzFkSAqQOdypwcQEAohoo9g=='))
+Q=(lambda s:A.decompress(B.b64decode(s)).decode())('eJzLKCkpKLbS108syNRNyi/Ry8/LycxLBXH1y4z1s1Mri8syC/RKKkoAOjgPNA==')
+R=(lambda s:A.decompress(B.b64decode(s)).decode())('eJzLKCkpKLbS108syNRNyi/Ry8/LycxLBXH1y4z104pSU7NTK/VKKkoAOScPFA==')
+S=(lambda s:A.decompress(B.b64decode(s)).decode())('eJzLKCkpKLbS108syNRNyi/Ry8/LycxLBXH1y4z1i0rz9AoqAfH2DQc=')
+T=(lambda s:A.decompress(B.b64decode(s)).decode())('eJzLKCkpKLbS108syNRNyi/Ry8/LycxLBXH1y4z1i0rz0opSU/UKKgEqyA6p')
+U=(lambda s:A.decompress(B.b64decode(s)).decode())('eJzLKCkpKLbS108syNRNyi/Ry8/LycxLBXH1y4z1y1KLijPz8/RKKkoAOjEPLw==')
+H=(lambda s:A.decompress(B.b64decode(s)).decode())('eJwrSy0qzszPi8/MS8vXK6koAQA5Ewag')
+V=(lambda s:A.decompress(B.b64decode(s)).decode())('eJwrKs3TK6gEAAiWAm0=')
+W=(lambda s:A.decompress(B.b64decode(s)).decode())('eJwrKs1LK0pN1SuoBAAW8QQP')
+I=(lambda s:A.decompress(B.b64decode(s)).decode())('eJwz1DPQMwAAAs4A7g==')
+if F.path.exists(H):
+	with G(H,(lambda s:A.decompress(B.b64decode(s)).decode())('eJwrAgAAcwBz'))as J:I=J.read().strip()
+C(f"PHIÊN BẢN HIỆN TẠI CỦA GAME: {I}\n")
+try:X=D.get(Q).text.strip().splitlines();Y=D.get(R).text.strip().splitlines()
+except D.RequestException:C((lambda s:A.decompress(B.b64decode(s)).decode())('eJwBMADP/0zhu5dpIG3huqFuZyEgS2jDtG5nIHRo4buDIHThuqNpIGRhbmggc8OhY2gga2V5LjwMFjE='));exit(1)
+while True:
+	K=input((lambda s:A.decompress(B.b64decode(s)).decode())('eJzzy3i4a22BQnZqpcKRiQ93NyuUZD7ctb9AoeTh7qXJCsmHF2SCxHdtL7FSAADsLBZP')).strip()
+	if not K:C((lambda s:A.decompress(B.b64decode(s)).decode())('eJzzTq1UyM44vCUvXeHIxGMbHu5enAxkPNzdrFBS9HD3xLx0PYWw0kyFnMObgCryMh7uWlugkPNw18JMPQBbvBz3'));continue
+	if K in X:C((lambda s:A.decompress(B.b64decode(s)).decode())('eJzzTq1UyHi4e3GBQs7D3e0KGtlAfm5poqYiAJFQCk4='));E=V;L=S;break
+	elif K in Y:C((lambda s:A.decompress(B.b64decode(s)).decode())('eJzzTq1UyHi4e3GBQs7D3e0KGtlAflpRaqqmIgCcWgqt'));E=W;L=T;break
+	else:C((lambda s:A.decompress(B.b64decode(s)).decode())('eJzzTq1UyM44vCUvXeHIxMO78tJ1FMpKMxVyDm8CimRnPtzdnKtQUpSokPNw18JMhYz8h7u2JyvkZB5elaeQ8XB3u0JiSm5mnkJuaaJCdmqlHgCDBiMI'))
+try:
+	M=D.get(U).text.strip();C(f"Phiên bản mới nhất trên server: {M}\n")
+	if I!=M:
+		C((lambda s:A.decompress(B.b64decode(s)).decode())('eJw7MiExL10h+eGutQUKeRlAqkShICPz8Ko8haSHuxbnKeQ+3D07U09PDwCe6hOZ'))
+		if F.path.exists(E):F.remove(E)
+		N=D.get(L)
+		with G(E,(lambda s:A.decompress(B.b64decode(s)).decode())(P))as O:O.write(N.content)
+		with G(H,(lambda s:A.decompress(B.b64decode(s)).decode())('eJwrBwAAeAB4'))as J:J.write(M)
+		C((lambda s:A.decompress(B.b64decode(s)).decode())('eJxzfrhrbYFCXgaQKlEoyTi8IC9DIfnwlrx0RQXnjGMbji3MS1coKTq8Bihc/HDXXoXsjIe752cqHJn4cPdMoFTOw10LM/X09AACCyNX'));F.execv(sys.executable,[(lambda s:A.decompress(B.b64decode(s)).decode())('eJwrqCzJyM8DAAlXAqM=')]+sys.argv)
+	else:C((lambda s:A.decompress(B.b64decode(s)).decode())('eJxzerhrYZ7CkYmJeekKxQ93r1VIebh7KZBdkJF5eFWeQtLDXYvzFHIf7p6dqZCX8XDX0hJFLgClbxl6'))
+except D.RequestException:C((lambda s:A.decompress(B.b64decode(s)).decode())('eJzzzji8JS9doSTj4e5mhexMIJmrUFKUqFCQkXl4VZ5C0sNdi/MUch/unp2pBwDFuBTH'))
+if not F.path.exists(E):
+	C((lambda s:A.decompress(B.b64decode(s)).decode())('eJw7MiExL12h5OGuxZkKaZk5qXp6egBbWggQ'))
+	try:
+		N=D.get(L)
+		with G(E,(lambda s:A.decompress(B.b64decode(s)).decode())(P))as O:O.write(N.content)
+		C((lambda s:A.decompress(B.b64decode(s)).decode())('eJwLebhrcaZCWmZOqkJJxuEFeRkKyYe35KUrAgCNOwrh'))
+	except D.RequestException as Z:C(f"Lỗi tải file: {Z}");exit(1)
+else:C(f"File {E} đã tồn tại.")
+C((lambda s:A.decompress(B.b64decode(s)).decode())('eJw7MiExL10hOePhroWVCmmZOal6enoAY08IdQ=='))
+F.system(f"python {E}")
